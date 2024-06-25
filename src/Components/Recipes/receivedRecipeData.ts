f6ba4d795d0305604cce44127c4bd9b73e750646
Christian <ps5christian@hotmail.com>
2024-06-25 09:49:09 +0200

    Made code that sorts recipes into meal types

--- src/Components/Recipes/receivedRecipeData.ts
+++ src/Components/Recipes/receivedRecipeData.ts
@@ -1,3 +1,71 @@
-export const receivedRecipeData = async (recipes: object) => {
-    console.log(recipes);   
-}
+export const receivedRecipeData = async (allRecipes: any) => {
+  enum MealType {
+    Dinner = "Dinner",
+    Lunch = "Lunch",
+    SideDish = "Side Dish",
+    Appetizer = "Appetizer",
+    Snacks = "Snack",
+    Breakfast = "Breakfast",
+    Beverage = "Beverage",
+    Dessert = "Dessert",
+  }
+
+  let dinnerArray: any[] = [];
+  let lunchArray: any[] = [];
+  let sideDishArray: any[] = [];
+  let appetizerArray: any[] = [];
+  let snacksArray: any[] = [];
+  let breakfastArray: any[] = [];
+  let beverageArray: any[] = [];
+  let dessertArray: any[] = [];
+  let miscArray: any[] = [];
+
+  allRecipes.recipes.map((recipe: any) => {
+    let mealTypeArray = recipe.mealType;
+    mealTypeArray.forEach((mealType: MealType) => {
+      const sortMealTypes = (mealType: MealType) => {
+        switch (mealType) {
+          case MealType.Dinner:
+            dinnerArray.push(recipe);
+            break;
+
+          case MealType.Lunch:
+            lunchArray.push(recipe);
+            break;
+
+          case MealType.SideDish:
+            sideDishArray.push(recipe);
+            break;
+
+          case MealType.Appetizer:
+            appetizerArray.push(recipe);
+            break;
+
+          case MealType.Snacks:
+            snacksArray.push(recipe);
+            break;
+
+          case MealType.Breakfast:
+            breakfastArray.push(recipe);
+            break;
+
+          case MealType.Beverage:
+            beverageArray.push(recipe);
+            break;
+
+          case MealType.Dessert:
+            dessertArray.push(recipe);
+            break;
+
+          default:
+            miscArray.push(recipe);
+            break;
+        }
+      };
+      sortMealTypes(mealType);
+    });
+  });
+  console.log(dinnerArray);
+  console.log(lunchArray);
+  console.log(miscArray);
+};
